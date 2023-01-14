2aa86eee99fcbeddd55e82b47ee6011e58c0e06e
parkpoomd <parkpoom.d@gmail.com>
2023-01-14 10:17:28 +0700

    up projects

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Features } from "./components/Features";
+import { Header } from "./components/Header";
+import { Pricing } from "./components/Pricing";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Pricing />
+      <Features />
+    </div>
+  );
+}
+
+export default App;
